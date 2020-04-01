a550727fc6f82f774cf6f2d5486650f324dbdbe2
Kevin Sparling <kevinsparling@gmail.com>
2020-04-01 17:25:05 +0800

    super basic image gallery

---
+++ src/components/PhotoGallery.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <ul class="gallery">
+      <li v-for="n in 5" :key="n">
+        <img @click="highlight"
+          :src="require('~/assets/images/' + n + '.jpg')">
+      </li>
+    </ul>
+    <div id="frame">
+      <img
+        :src="this.theatrical"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PhotoGallery',
+  data() {
+    return {
+      theatrical: ""
+    }
+  },
+  methods: {
+    highlight() {
+      event.target.id = "theater"
+      this.theatrical = event.target.src;
+
+      let eventIterator = event.target.parentNode
+      while (eventIterator.previousElementSibling != null) {
+        eventIterator.previousElementSibling.getElementsByTagName('img')[0].id = ""
+        eventIterator = eventIterator.previousElementSibling
+      }
+      eventIterator = event.target.parentNode
+      while (eventIterator.nextElementSibling != null) {
+        eventIterator.nextElementSibling.getElementsByTagName('img')[0].id = ""
+        eventIterator = eventIterator.nextElementSibling
+      }
+  }
+}
+
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-tope: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+.gallery {
+  display: flex;
+  justify-content: space-around;
+}
+img {
+  width: 20%;
+}
+#theater {
+  width: 40%;
+}
+#frame img {
+  width: 80%;
+}
+</style>
--- src/pages/class.vue
+++ src/pages/class.vue
@@ -2,14 +2,19 @@
   <Layout>
     <div class="container mx-auto">
       <h1 class="text-3xl">
+        Class
       </h1>
+      <PhotoGallery />
     </div>
   </Layout>
 </template>
 
 <script>
+import PhotoGallery from "../components/PhotoGallery"
+
 export default {
   components: {
+    PhotoGallery
   }
 }
 </script>
