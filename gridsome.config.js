166c29ff121fb1ebcbcf7207453495bf0bf723a2
Kevin Sparling <kevinsparling@gmail.com>
2020-05-05 20:35:57 +0800

    worked on the header a bit, working on the students

--- gridsome.config.js
+++ gridsome.config.js
@@ -31,7 +31,7 @@ module.exports = {
       options: {
         apiURL: 'http://localhost:1337',
         queryLimit: 1000, // Defaults to 100
-        contentTypes: ['student'],
+        contentTypes: ['student', 'phoneme', 'author'],
         loginData: {
           identifier: '',
           password: ''
---
+++ src/components/Student.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="student" class="flex flex-wrap items-center justify-center sm:justify-left border-blue-300 w-full mt-10 py-5 sm:px-16">
+
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  name: "Student",
+  props: ['student'],
+  computed: {
+    avatar() {
+      return `/students/images/${this.student.name}.jpg`
+    }
+  },
+  methods: {
+    titleCase(str) {
+      return str.replace('-', ' ')
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ')
+    },
+    imageLoadError(e) {
+      e.target.src = `/students/images/default.jpg`
+    }
+  }
+}
+</script>
