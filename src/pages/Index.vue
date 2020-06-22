8c9f48dd59b651902867f57508873b1f464cf6d6
Kevin Sparling <kevinsparling@gmail.com>
2020-06-22 23:04:37 +0800

    added BlogPostCard component, edited index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,93 @@
 <template>
-  <Layout>
+  <Layout title="David Li Portfolio">
+    <br />
+    <br />
+    <div class="container">
+      <h1 class="title">Gridsome</h1>
+      <h2 class="subtitle">Steps to get started</h2>
+    </div>
+    <hr />
+    <br />
+    <div class="container">
+    <b-steps
+      v-model="activeStep"
+    >
+      <b-step-item label="Installation" icon="npm">
+        <ul>
+          <li> Install gridsome by running <i> npm install -g gridsome </i> </li>
+          <li> Then clone this repo by running <i> git clone https://github.com/FriendlyUser/gridsome-starter-buefy.git</i> </li>
+        </ul>
+      </b-step-item>
+      <b-step-item label="Configuration" icon="web">
+        <ul>
+          <li> Update <i>gridsome.config.js</i> with unique site name and hosting details. </li>
+          <li> Change formspree to use your personal email address. </li>
+        </ul>
+      </b-step-item>
+      <b-step-item label="Deployment" icon="azure">
+        Deployment can be handled using azure pipelines, additionally considering adding more customization.
+      </b-step-item>
+    </b-steps>
+    </div>
+    <div class="container">
+      <h3 class="title">Contact Me</h3>
+      <section>
+        <form action="https://formspree.io/davidli012345@gmail.com" method="POST">
+          <b-field horizontal label="Subject" type="is-danger" message="Please enter a subject">
+            <b-input name="subject" expanded></b-input>
+          </b-field>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+          <b-field horizontal label="From">
+            <b-input name="name" placeholder="Name" expanded></b-input>
+            <b-input name="email" type="email" placeholder="nobody@nowhere.com" expanded></b-input>
+          </b-field>
 
-    <h1>Hello, world!</h1>
+          <b-field horizontal label="Topic">
+            <b-select placeholder="Select a topic">
+              <option value="1">Bulma</option>
+              <option value="2">Vue.js</option>
+              <option value="3">Buefy</option>
+            </b-select>
+          </b-field>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+          <b-field horizontal label="Message">
+            <b-input type="textarea" name="text"></b-input>
+          </b-field>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+          <b-field horizontal><!-- Label left empty for spacing -->
+            <p class="control">
+              <button class="button is-primary">
+                Send message
+              </button>
+            </p>
+          </b-field>
+        </form>
+      </section>
+    </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Gridsome Starter Buefy</strong> by <a href="https://FriendlyUser.github.io">David Li</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+      </div>
+    </footer>
   </Layout>
 </template>
 
+
 <script>
+import Layout from '~/layouts/Default.vue'
+
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
+  data() {
+    return {
+      activeStep: 0,
+    }
+  },
+  components: {
+    Layout
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
