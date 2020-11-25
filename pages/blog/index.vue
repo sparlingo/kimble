9b6f3bb84d318508112361c65e348ca9cefd420f
Kevin Sparling <kevinsparling@gmail.com>
2020-11-25 23:11:16 +0800

    blog index works

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,16 +1,23 @@
 <template>
-
+  <div id="allPosts">
+    <div class="card" style="width: 20rem;" v-for="post in posts" :key="_id">
+      <img src="https://picsum.photos/384" alt="Card example image">
+      <div class="card-body">
+        <h4 class="card-title">{{post.title}}</h4>
+        <h5 class="card-subtitle">Nice looking subtitle.</h5>
+        <p class="card-text">{{post.body}}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { groq } from '@nuxtjs/sanity'
-
-const query = groq`*[_type == "post"][0].title`
 
 export default {
-  mounted() {
-    this.observable = this.$sanity.client.listen(query)
-  },
-  data: () => ({ observable: null})
+  asyncData({ $sanity }) {
+    const query = '{ "posts": *[_type == "post"] }'
+    return $sanity.fetch(query)
+  }
 }
+
 </script>
