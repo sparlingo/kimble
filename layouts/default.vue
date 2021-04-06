25aee8ab9da7c83b77e335b7decf07b09d68a408
Kevin Sparling <kevinsparling@gmail.com>
2021-04-06 12:36:45 +0800

    updated route for classes, fixed footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,15 +8,17 @@
         </main>
       </transition>
     </div>
-    <!-- <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <strong>Kimble</strong> by <a href="https://sparlingo.github.io">Kevin Sparling</a>. The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-        </p>
-      </div>
-    </footer> -->
+    <div class="container">
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Kimble</strong> by <a href="https://sparlingo.github.io">Kevin Sparling</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </p>
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
---
+++ pages/classes/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="allGroups">
+    <div class="card" style="width: 20rem;" v-for="group in groups" :key="_id">
+      <img src="https://picsum.photos/384" alt="Card example image">
+      <div class="card-body">
+        <h4 class="card-title">{{group.title}}</h4>
+        <h5 class="card-subtitle">Nice looking subtitle.</h5>
+        <p class="card-text">{{group.body}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { groq } from '@nuxtjs/sanity'
+
+const query = groq`{ "groups": *[_type == "group"] }`
+
+export default {
+  asyncData({ $sanity }) {
+    return $sanity.fetch(query)
+  }
+}
+
+</script>
